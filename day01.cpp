470e9bb5d3a339873e2f0f06a69cfdb57a49237c
klbarrus <klbarrus@yahoo.com>
2019-03-08 01:34:45 -0500

    use <algorithm>

--- day01.cpp
+++ day01.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
 using namespace std;
 
 std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ") {
@@ -10,46 +11,41 @@ std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ") {
     return str;
 }
 
+int captcha(std::string& first, std::string& second) {
+    int sum = 0;
+    auto xi = first.begin();
+    auto yi = second.begin();
+    while (xi != first.end() && yi != second.end()) {
+        if (*xi == *yi) {
+            sum += int(*xi) - int('0');
+        }
+        *xi++;
+        *yi++;
+    }
+    return sum;
+}
+
 int main(int argc, char* argv[]) {
     if (argc > 1) {
-        fstream puzz(argv[1], ios::in);
-        ostringstream puzz_s;
-        puzz_s << puzz.rdbuf();
-        string puzz_digits = puzz_s.str();
-        puzz_digits = rtrim(puzz_digits);
-        puzz.close();
+        fstream puzzle_f(argv[1], ios::in);
+        ostringstream puzzle_s;
+        puzzle_s << puzzle_f.rdbuf();
+        string puzz_dig = puzzle_s.str();
+        puzz_dig = rtrim(puzz_dig);
+        puzzle_f.close();
 
-        int sum{0};
-        char prev{0};
+        unsigned long len = puzz_dig.length();
+        string puzz_one = puzz_dig;
+        string puzz_two = puzz_dig;
+        std::rotate(puzz_one.begin(), puzz_one.begin() + 1, puzz_one.end());
+        std::rotate(puzz_two.begin(), puzz_two.begin() + len / 2, puzz_two.end());
 
-        for (char& digit : puzz_digits) {
-            if (digit == prev) {
-                sum += int(prev) - int('0');
-            }
-            prev = digit;
-        }
+        // compare original string (file contents) with rotated string(s), char by char,
+        // and look for matches
+        // doesn't seem to be a sequence-zip in c++ 17 yet
 
-        char first = puzz_digits[0];
-        char last = puzz_digits.back();
-
-        if (first == last) {
-            sum += int(first) - int('0');
-        }
-
-        cout << "part 1: " << sum << "\n";
-
-        unsigned long digits_len = puzz_digits.length();
-        sum = 0;
-
-        for (unsigned long i = 0; i < digits_len; ++i) {
-            char curr = puzz_digits[i];
-            char circle = puzz_digits[(i + digits_len / 2) % digits_len];
-            if (curr == circle) {
-                sum += int(curr) - int('0');
-            }
-        }
-
-        cout << "part 2: " << sum << "\n";
+        cout << "part 1: " << captcha(puzz_dig, puzz_one) << "\n";
+        cout << "part 2: " << captcha(puzz_dig, puzz_two) << "\n";
     }
 
     return 0;
