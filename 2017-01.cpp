f81c834776f188b9c33a7e90189d31a8a5c13e53
klbarrus <klbarrus@yahoo.com>
2019-03-14 22:52:05 -0400

    cleanup

--- 2017-01.cpp
+++ 2017-01.cpp
@@ -4,7 +4,6 @@
 #include <fstream>
 #include <sstream>
 #include <algorithm>
-using namespace std;
 
 std::string& rtrim(std::string& str, const std::string& chars = "\t\n\v\f\r ") {
     str.erase(str.find_last_not_of(chars) + 1);
@@ -26,27 +25,29 @@ int captcha(std::string& first, std::string& second) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        fstream puzzle_f(argv[1], ios::in);
-        ostringstream puzzle_s;
-        puzzle_s << puzzle_f.rdbuf();
-        string puzz_dig = puzzle_s.str();
-        puzz_dig = rtrim(puzz_dig);
-        puzzle_f.close();
+    std::ifstream puzzle_in;
+    if (argc > 1)
+        puzzle_in.open(argv[1]);
+    else
+	    puzzle_in.open("../2017-01.txt");
+    std::ostringstream puzzle_s;
+    puzzle_s << puzzle_in.rdbuf();
+    std::string puzz_dig = puzzle_s.str();
+    puzz_dig = rtrim(puzz_dig);
+    puzzle_in.close();
 
-        unsigned long len = puzz_dig.length();
-        string puzz_one = puzz_dig;
-        string puzz_two = puzz_dig;
-        std::rotate(puzz_one.begin(), puzz_one.begin() + 1, puzz_one.end());
-        std::rotate(puzz_two.begin(), puzz_two.begin() + len / 2, puzz_two.end());
+    unsigned long len = puzz_dig.length();
+    std::string puzz_one = puzz_dig;
+    std::string puzz_two = puzz_dig;
+    std::rotate(puzz_one.begin(), puzz_one.begin() + 1, puzz_one.end());
+    std::rotate(puzz_two.begin(), puzz_two.begin() + len / 2, puzz_two.end());
 
-        // compare original string (file contents) with rotated string(s), char by char,
-        // and look for matches
-        // doesn't seem to be a sequence-zip in c++ 17 yet
+    // compare original string (file contents) with rotated string(s), char by char,
+    // and look for matches
+    // doesn't seem to be a sequence-zip in c++ 17 yet
 
-        cout << "part 1: " << captcha(puzz_dig, puzz_one) << "\n";
-        cout << "part 2: " << captcha(puzz_dig, puzz_two) << "\n";
-    }
+    std::cout << "part 1: " << captcha(puzz_dig, puzz_one) << "\n";
+    std::cout << "part 2: " << captcha(puzz_dig, puzz_two) << "\n";
 
     return 0;
 }
