0a4596e7e96828012900065bd6f505976ac011dd
klbarrus <klbarrus@yahoo.com>
2019-03-14 23:01:04 -0400

    cleanup

--- 2017-03.cpp
+++ 2017-03.cpp
@@ -4,27 +4,30 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
-    if (argc > 1) {
-        unsigned int start;
-        std::fstream puzzle_in(argv[1]);
-        puzzle_in >> start;
-        puzzle_in.close();
+    std::ifstream puzzle_in;
+    if (argc > 1)
+        puzzle_in.open(argv[1]);
+    else
+        puzzle_in.open("../2017-03.txt");
 
-        unsigned int res = 0;
-        unsigned int oddsq = 3;
-        while (oddsq * oddsq < start)
-            oddsq += 2;
+    unsigned int start;
+    puzzle_in >> start;
+    puzzle_in.close();
 
-        unsigned int fs = (oddsq * oddsq - start) % (oddsq - 1);
-        if (fs > (oddsq - 1) / 2)
-            res = fs;
-        else
-            res = oddsq - 1 - fs;
+    unsigned int res = 0;
+    unsigned int oddsq = 3;
+    while (oddsq * oddsq < start)
+        oddsq += 2;
 
-        std::cout << "part 1: " << res << '\n';
+    unsigned int fs = (oddsq * oddsq - start) % (oddsq - 1);
+    if (fs > (oddsq - 1) / 2)
+        res = fs;
+    else
+        res = oddsq - 1 - fs;
+
+    std::cout << "part 1: " << res << '\n';
 
 	// for part 2, see OEIS sequence 141481 (https://oeis.org/A141481)
-    }
 
     return 0;
 }
