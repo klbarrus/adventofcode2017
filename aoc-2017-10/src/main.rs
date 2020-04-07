357e551ab971ea171dc55fe1f95af8c5b990dd21
Karl Barrus <klbarrus@yahoo.com>
2020-04-07 16:27:47 -0400

    day 10 part 1 refactor

--- aoc-2017-10/src/main.rs
+++ aoc-2017-10/src/main.rs
@@ -3,6 +3,21 @@
 use aoclib;
 use std::io;
 
+fn select_reverse(list: &mut Vec<usize>, cur_pos: u16, len: u16) -> () {
+    let mut head = cur_pos;
+    let mut tail = (head + len - 1) % 256;
+    let count = len / 2;
+    for _ in 0..count {
+        list.swap(head as usize, tail as usize);
+        head = (head + 1) % 256;
+        if tail == 0 {
+            tail = 255;
+        } else {
+            tail = tail - 1;
+        }
+    }
+}
+
 fn main() -> io::Result<()> {
     let filestr = aoclib::read_file_string(&"2017-10.txt")?;
 
@@ -19,19 +34,9 @@ fn main() -> io::Result<()> {
     for len_str in split {
         let len = len_str.parse::<u16>().unwrap();
 
+        // only need to select-reverse if the length is greater than 1
         if len > 1 {
-            let mut start = cur_pos;
-            let mut end = (start + len - 1) % 256;
-            let count = len / 2;
-            for _ in 0..count {
-                list.swap(start as usize, end as usize);
-                start = (start + 1) % 256;
-                if end == 0 {
-                    end = 255;
-                } else {
-                    end = end - 1;
-                }
-            }
+            select_reverse(&mut list, cur_pos, len);
         }
 
         cur_pos = (cur_pos + len + skip_size) % 256;
